b239ae62aba3e9c0da4d756f80bc17d78d35e627
root <root@ari.localdomain>
2017-09-17 15:32:47 +0300

    file upload

--- src/main/java/com/kodgemisi/DemoApplication.java
+++ src/main/java/com/kodgemisi/DemoApplication.java
@@ -1,18 +1,28 @@
 package com.kodgemisi;
 
 
+import com.kodgemisi.service.impl.StorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
 public class DemoApplication {
+
+
+	@Resource
+	StorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
+
 
 
 }
--- src/main/java/com/kodgemisi/controller/ApplicantController.java
+++ src/main/java/com/kodgemisi/controller/ApplicantController.java
@@ -3,14 +3,18 @@ package com.kodgemisi.controller;
 import com.kodgemisi.model.ApplicantDTO;
 import com.kodgemisi.service.ApplicantService;
 import com.kodgemisi.service.JobService;
+import com.kodgemisi.service.impl.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
@@ -21,6 +25,11 @@ public class ApplicantController {
 
     private final ApplicantService applicantService;
     private final JobService jobService;
+
+    @Autowired
+    StorageService storageService;
+
+    List<String> files = new ArrayList<String>();
 
 
     @Autowired
@@ -41,9 +50,20 @@ public class ApplicantController {
 
 
     @RequestMapping(value = "/applicant/new",method = RequestMethod.POST)
-    public String formHandle(@Valid @ModelAttribute("jobApplicationForm") ApplicantDTO dto, BindingResult bindingResult){
+    public String formHandle(@Valid @ModelAttribute("jobApplicationForm") ApplicantDTO dto, BindingResult bindingResult,
+                             @RequestParam("file") MultipartFile file, Model model){
         if(bindingResult.hasErrors())
             return "applicantForm";
+
+        try {
+            storageService.store(file);
+            model.addAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + "!");
+            files.add(file.getOriginalFilename());
+        } catch (Exception e) {
+            model.addAttribute("message", "FAIL to upload " + file.getOriginalFilename() + "!");
+        }
+
+
         applicantService.assignApplicantWithJob(dto);
         return "redirect:/jobs";
     }
